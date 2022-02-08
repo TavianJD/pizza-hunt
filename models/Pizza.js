62b565fd2c769ea29be43ff7efe221dfe3341663
hatetavian <tavianjdawson123@gmail.com>
2022-02-08 16:51:40 -0600

    reply page

--- models/Pizza.js
+++ models/Pizza.js
@@ -40,8 +40,7 @@ const Pizza = model('Pizza', PizzaSchema);
 
 // get total count of comments and replies on retrieval
 PizzaSchema.virtual('commentCount').get(function() {
-    return this.comments.length;
+    return this.comments.reduce((total, comment) => total + comment.replies.length + 1, 0);
   });
-
 // expoort the pizza model
 module.exports = Pizza;
