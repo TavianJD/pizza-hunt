75288796a5fac6f3a00840dc0175fed08fb76eb2
hatetavian <tavianjdawson123@gmail.com>
2022-02-08 16:22:02 -0600

    adding comment functionality and display

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,25 +1,47 @@
 const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
-const PizzaSchema = new Schema({
-    pizzaName: {
+const PizzaSchema = new Schema(
+    {
+      pizzaName: {
         type: String
-    },
-    createdBy: {
+      },
+      createdBy: {
         type: String
-    },
-    createdAt: {
+      },
+      createdAt: {
         type: Date,
-        default: Date.now
-    },
-    size: {
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal)
+      },
+      size: {
         type: String,
         default: 'Large'
-    },
-    toppings: []
-});
+      },
+      comments: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'Comment'
+        }
+      ],
 
+      toppings: [],
+    },
+    {
+      toJSON: {
+        virtuals: true,
+        getters: true
+      },
+      id: false
+    }
+  );
 // create the pizza model using the PizzaSchema
 const Pizza = model('Pizza', PizzaSchema);
+
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function() {
+    return this.comments.length;
+  });
 
 // expoort the pizza model
 module.exports = Pizza;
